c12acc9f34e5ac69d7ea5e95fb87e995ef349da3
goatboy333 <danielcurtis333@gmail.com>
2023-03-31 16:56:44 +1100

    bear image added to bear directory

---
+++ mygame/app/bear.rb
@@ -0,0 +1,27 @@
+class Bear < Creature
+
+    def initialize(args)
+        super
+        @x = 100
+        @y = 50
+        @health = 10
+        @damage = 3
+        @speed = 3
+        @move = true
+        @count = 3
+        @hold_for = 10
+        @width = 76
+        @creature_hash = {
+            x: @x,
+            y: @y,
+            w: 76 * 3.5,
+            h: 64 * 3.5,
+            path: 'sprites/bear/black_bear_2.png',
+            source_x: 76,
+            source_y: 0,
+            source_w: @width,
+            source_h: 64,
+            flip_horizontally: false,
+          } 
+    end
+end
