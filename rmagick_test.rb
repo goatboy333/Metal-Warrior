962501f3cfc2540e508b29298d0ba12b90095526
Michael Pope <map7777@gmail.com>
2023-06-08 23:29:32 +1000

    RMagick tests

--- rmagick_test.rb
+++ rmagick_test.rb
@@ -1,5 +1,27 @@
 require 'rmagick'
 
+sheet = Magick::Image.read("sheet.png").first
+width = sheet.columns
+height = sheet.rows
+
+puts "Original: #{width} x #{height}"
+
+index = 10
+
+y = 11
+
+sheet.crop!(288 * index, 128 * (15-y), 288, 128)
+
+sheet.trim!
+
+# sheet.write("sheet_output.png")
+width = sheet.columns
+height = sheet.rows
+
+puts "New: #{width} x #{height}"
+
+exit
+
 image = Magick::Image.read('test.png').first
 width = image.columns
 height = image.rows
