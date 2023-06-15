0a4af10e8fecfe76a341fc797a917da3a0c2e847
Michael Pope <map7777@gmail.com>
2023-06-15 21:40:42 +1000

    Add idle sprite sheet

--- mygame/app/player.rb
+++ mygame/app/player.rb
@@ -8,13 +8,14 @@ class Player
     @health = 100
     @x = x
     @y = y
-    @w = 288 * 2.5
-    @h = 128 * 2.5
+    @w = 47 * 2.5
+    @h = 42 * 2.5
     # @path = 'sprites/villager/Run/villager_run.png'
-    @path ='/sprites/bladekeeper/spritesheets/metal_bladekeeper_FREE_v1.1_SpriteSheet_288x128.png'
-    @source_w = 288
-    @source_h = 128
-    @action = {idle: 15 * @source_h,
+    # @path ='/sprites/bladekeeper/spritesheets/metal_bladekeeper_FREE_v1.1_SpriteSheet_288x128.png'
+    @path ='/sprites/bladekeeper/mog/idlesheet.png'
+    @source_w = 47
+    @source_h = 42
+    @action = {idle: 0 * @source_h,
                run: 14 * @source_h,
                jump: 11 * @source_h,
                attack: 6 * @source_h,
