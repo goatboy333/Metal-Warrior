cfe4b073efc6fcf6c0f06f191853fcbdbba2fcae
Michael Pope <map7777@gmail.com>
2023-06-29 23:43:14 +1000

    Turn wolf dead if health is zero or below

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -156,7 +156,8 @@ class MyGame
       creatures = [player] + @wolves
 
       creatures.each do |sprite|
-        outputs.sprites << sprite if sprite.health > 0
+        sprite.dead if sprite.health <= 0
+        outputs.sprites << sprite
         # outputs.borders << sprite
         # outputs.borders << {x: sprite.x + (sprite.w / 3), y: sprite.y + (sprite.h + 10), w: 50, h: 10} if sprite.health > 0
         outputs.primitives << {x: sprite.x + (sprite.w / 3), y: sprite.y + (sprite.h + 10), w: 50 * (sprite.health / sprite.max_health), h: 10, r: 255, g: 255, b:0, a: 255}.solid if sprite.health > 0
